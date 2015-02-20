5765687b9a052abbcd757e5123e3b3fd6982edb8
root at v1 <root@v1>
2015-02-20 09:27:53 +0000

    nginx-pull

--- ghost-git/backup.sh
+++ ghost-git/backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /data
+git config --local user.email "backup@indiehosters"
+git config --local user.name "IndieHosters backup"
 while true; do
   git add *
   git commit -am"backup `date`"
